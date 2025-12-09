0715cde6e84a5e9a585d49deb9eeb93103b53c2d
sch2015 <564352527@qq.com>
2025-12-09 08:53:29 +0800

    6

--- a.js
+++ a.js
@@ -1,7 +1,14 @@
 function plus(a, b) {
     return a + b;
 }
+function daysBetweenDates(date1, date2) {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
 
+    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+    return diffDays;
+}
 plus = function (a, b) {
     return a + b + 1;
 }
